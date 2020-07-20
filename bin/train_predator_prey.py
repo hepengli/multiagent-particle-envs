a123ce62f81b1a52bcdabdce9bcaad6e43f736fc
hepengli <hepengli@uri.edu>
2020-07-20 13:43:04 -0400

    minor

--- bin/train_predator_prey.py
+++ bin/train_predator_prey.py
@@ -11,11 +11,11 @@ def main():
     model = 'matrpo_vs_independent'
     seed = 1
     network_kwargs = {'num_layers': 2, 'num_hidden': 128, 'activation': 'selu'}
-    reward_path = '/home/lihepeng/Documents/Github/multiagent-particle-envs/results/training/{}/{}/s{}'.format(env_id, model, seed)
-    load_path = '/home/lihepeng/Documents/Github/multiagent-particle-envs/results/graphs/{}/{}/s{}'.format(env_id, model, seed)
+    reward_path = '/home/lihepeng/Documents/Github/multiagent_results/training/{}/{}/s{}'.format(env_id, model, seed)
+    load_path = '/home/lihepeng/Documents/Github/multiagent_results/graphs/{}/{}/s{}'.format(env_id, model, seed)
     agents = MATRPO(
         env_id=env_id,
-        nsteps=2000,
+        nsteps=200,
         network='mlp',
         num_env=10,
         admm_iter=[80,0],
