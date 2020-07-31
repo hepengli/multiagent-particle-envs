e5e80107546915d6e9e615d1b76ce68ab8ad0af6
hepengli <hepengli@uri.edu>
2020-07-31 09:26:31 -0400

    minor

--- bin/learning_curves.py
+++ bin/learning_curves.py
@@ -3,33 +3,15 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from baselines.bench.monitor import load_results
 
-def get_training_curve_1(model):
-    env_id = 'simple_catchup'
-    seeds = [1,2,3,4,5]
-    pred_results = []
-    prey_results = []
-    for seed in seeds:
-        reward_path = '/home/lihepeng/Documents/Github/multiagent-particle-envs/results/training/{}/{}/s{}'.format(env_id, model, seed)
-        df_train = load_results(reward_path)
-        pred_data = df_train[['r0','r1','r2']].sum(axis=1).values
-        pred_data = np.mean(pred_data.reshape(-1, 100), axis=1)
-        pred_results.append(pred_data)
-
-        prey_data = df_train[['r3']].sum(axis=1).values
-        prey_data = np.mean(prey_data.reshape(-1, 100), axis=1)
-        prey_results.append(prey_data)
-
-    return np.array(pred_results), np.array(prey_results)
-
-
-def get_training_curve(model):
-    env_id = 'simple_spread_6'
+def get_training_curve(model, num_agents=3):
+    env_id = 'simple_spread_{}'.format(num_agents)
+    agents = ['r{}'.format(i) for i in range(num_agents)]
     seeds = [1,2,3,4,5]
     results = []
     for seed in seeds:
-        reward_path = '/home/lihepeng/Documents/Github/multiagent-particle-envs/results/training/{}/{}/s{}'.format(env_id, model, seed)
+        reward_path = '/home/lihepeng/Documents/Github/results/training/{}/{}/s{}'.format(env_id, model, seed)
         df_train = load_results(reward_path)
-        data = df_train[['r0','r1','r2','r3','r4','r5']].mean(axis=1).values
+        data = df_train[agents].mean(axis=1).values
         data = np.mean(data.reshape(-1, 100), axis=1)
         results.append(data)
 
@@ -39,10 +21,48 @@ xdata = np.arange(200)
 models = ['ctrpo','matrpo','trpo']
 results = []
 for model in models:
-    results.append(get_training_curve(model))
+    results.append(get_training_curve(model, num_agents=6))
 
 fig = plt.figure()
-sns.tsplot(time=xdata, data=results[0], color='r', linestyle='-')
-sns.tsplot(time=xdata, data=results[1], color='g', linestyle='-')
-sns.tsplot(time=xdata, data=results[2], color='b', linestyle='-')
+sns.tsplot(time=xdata, data=results[0], color='r', linestyle='-', legend=True)
+sns.tsplot(time=xdata, data=results[1], color='g', linestyle='-', legend=True)
+sns.tsplot(time=xdata, data=results[2], color='b', linestyle='-', legend=True)
+plt.legend(labels=['Central TRPO', 'MATRPO', 'Independent TRPO'])
 plt.show()
+
+
+# Predator prey
+def get_training_curve_1(model):
+    env_id = 'simple_predator_prey'
+    seeds = [1,2,3]
+    pred_results = []
+    prey_results = []
+    for seed in seeds:
+        reward_path = '/home/lihepeng/Documents/Github/results/training/{}/{}/s{}'.format(env_id, model, seed)
+        df_train = load_results(reward_path)
+        pred_data = df_train[['r0','r1','r2','r3']].sum(axis=1).values[:500*150]
+        pred_data = np.mean(pred_data.reshape(-1, 150), axis=1)
+        pred_results.append(pred_data)
+
+        prey_data = df_train[['r4','r5','r6']].sum(axis=1).values[:500*150]
+        prey_data = np.mean(prey_data.reshape(-1, 150), axis=1)
+        prey_results.append(prey_data)
+
+    return np.array(pred_results), np.array(prey_results)
+
+
+xdata = np.arange(500)
+models = ['independent_vs_independent']
+pred_results, prey_results = [], []
+for model in models:
+    pred, prey = get_training_curve_1(model)
+    pred_results.append(pred)
+    prey_results.append(prey)
+
+fig = plt.figure()
+sns.tsplot(time=xdata, data=pred_results[0], color='r', linestyle='-', legend=True)
+# sns.tsplot(time=xdata, data=results[1], color='g', linestyle='-', legend=True)
+# sns.tsplot(time=xdata, data=results[2], color='b', linestyle='-', legend=True)
+# plt.legend(labels=['Central TRPO', 'MATRPO', 'Independent TRPO'])
+plt.show()
+
--- bin/play.py
+++ bin/play.py
@@ -6,22 +6,22 @@ from multiagent.matrpo import MATRPO
 
 def main():
     env_id = 'collector'
-    model = 'matrpo_vs_independent'
-    seed = 3
-    network_kwargs = {'num_layers': 2, 'num_hidden': 128, 'activation': 'selu'}
+    model = 'independent'
+    seed = 1
+    reward_path = '/home/lihepeng/Documents/Github/results/training/{}/{}/s{}'.format(env_id, model, seed)
     load_path = '/home/lihepeng/Documents/Github/results/graphs/{}/{}/s{}'.format(env_id, model, seed)
+    network_kwargs = {'num_layers': 2, 'num_hidden': 256, 'activation': 'tanh'}
     agents = MATRPO(
         env_id=env_id,
-        nsteps=2000,
+        nsteps=1000,
         network='mlp',
         num_env=1,
         admm_iter=[0,0],
         ob_clip_range=5.0,
-        # load_path=load_path,
+        load_path=load_path,
         seed=seed,
         info_keywords=tuple('r{}'.format(i) for i in range(8)),
         adv='independent',
-        agt='independent',
         **network_kwargs)
 
     agents.play()
